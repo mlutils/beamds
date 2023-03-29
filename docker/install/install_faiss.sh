1898033a6cc8375fefae309f5ddc1b3c46a09c82
elad <elad.sarafian@gmail.com>
2023-03-29 17:16:59 +0300

    work on Dockerfile

--- docker/install/install_faiss.sh
+++ docker/install/install_faiss.sh
@@ -6,5 +6,6 @@ git clone https://github.com/facebookresearch/faiss.git
 cd faiss/
 cmake -B build -DFAISS_ENABLE_GPU=ON -DFAISS_ENABLE_PYTHON=ON -DCMAKE_CUDA_ARCHITECTURES="90;89;87;86;80;75;72" .
 make -C build -j faiss
+make -C build -j swigfaiss
 (cd build/faiss/python && python setup.py install)
 cd ../
