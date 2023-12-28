17c86889f2c4536203e6174d01c1dcdd43ccde35
eladsar <elad.sarafian@gmail.com>
2023-12-28 21:57:09 +0200

    work on configurations.py

--- docker/build-lite-beam.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -f docker/Dockerfile_lightweight --tag beam:$(date '+%Y%m%d') . | tee -a /tmp/beam_build_$(date '+%Y%m%d').log
--- docker/install/install_mongo.sh
+++ docker/install/install_mongo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+apt install -y gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
 
 apt update
 apt install -y mongodb-org
