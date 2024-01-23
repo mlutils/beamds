67cece0d7d65aaf0db792d2e008f5017c4917f4c
eladsar <elad.sarafian@gmail.com>
2024-01-23 20:35:11 +0200

    tabular hpo conf

--- examples/tabular_hpo.py
+++ examples/tabular_hpo.py
@@ -35,8 +35,8 @@ if __name__ == '__main__':
     # kwargs_all['adult'] = dict(batch_size=128)
     # kwargs_all['helena'] = dict(batch_size=256)
     # kwargs_all['jannis'] = dict(batch_size=256)
-    kwargs_all['higgs_small'] = dict(batch_size=256)
-    # kwargs_all['aloi'] = dict(batch_size=256)
+    # kwargs_all['higgs_small'] = dict(batch_size=256)
+    kwargs_all['aloi'] = dict(batch_size=256)
     # kwargs_all['year'] = dict(batch_size=512)
     # kwargs_all['covtype'] = dict(batch_size=1024)
 
