4e0c8ae34428d56c7f5e011d16bec3482f2bc2ec
Yossi <yossi@dayo-tech.com>
2024-01-28 08:09:34 +0200

    add functionalities to k8s

--- examples/orchestration_example.py
+++ examples/orchestration_example.py
@@ -1,24 +1,79 @@
+from typing import List
+
 from src.beam import beam_logger as logger
 from src.beam.orchestration import BeamK8S
+from kubernetes import client, config
+
+api_url = "https://api.kh-dev.dt.local:6443"
+api_token = "sha256~rcBAFKblEq83dkshTqhMeCa8Gl2kUSHP4QunM8ukxJI"
+project_name = "kleiner"
+image_name = "harbor.dt.local/public/beam:20231221"
+ports: list[int] = [80, 22,]
+labels = {"app": "beamds"}
+deployment_name = "beamds"
+namespace = "kleiner"
+replicas = 1
+args = ["63"]  # Container arguments
+env = {"TEST": "test"}  # Container environment variables
+
 print('hello world')
-k8s = BeamK8S(api_url="https://api.kh-dev.dt.local:6443", api_token="sha256~5BjsBmhuK-bFa5shjVAqkRqLAIpUTxHXbIqE756L9NY")
-print("API URL:", k8s.api_url)
-print("API Token:", k8s.api_token)
 
-#k8s.client()
-k8s.get_pods(namespace="kleiner")
+print("API URL:", api_url)
+print("API Token:", api_token)
 
-for ns in k8s.namespaces.items:
-    logger.info(f"Namespace: {ns.metadata.name}")
+# the order of the VARS is important!!! (see BeamK8S class)
+k8s = BeamK8S(
+    api_url=api_url,
+    api_token=api_token,
+    project_name=project_name,
+    namespace=namespace,
+    replicas=1,  # Ensure this is an integer
+    labels=labels,
+    image_name=image_name,
+    deployment_name=deployment_name,
+    ports=ports,
+    env=env
+)
+#k8s = k8s.create_deployment()
+k8s = k8s.apply_deployment()
 
-k8s.get_deployments(namespace="kleiner")
+# k8s.get_pods(namespace="kleiner")
+# for ns in k8s.namespaces.items:
+#     logger.info(f"Namespace: {ns.metadata.name}")
+#
+# k8s.get_deployments(namespace="kleiner")
 
-k8s.generate_yaml_config('/tmp/my_conf.yaml', namespace='kleiner', deployment_name='yoss', image='beam',
-                         replicas=1, port=80, cpu_request=0.1, cpu_limit=1,)
 
-k8s.generate_file_config('/tmp/my_conf.json', namespace='kleiner', deployment_name='yoss', image='beam',
-                         replicas=1, port=80, cpu_request=0.1, cpu_limit=1,)
 
+
+#k8s = BeamK8S(api_url="https://api.kh-dev.dt.local:6443", api_token="sha256~rcBAFKblEq83dkshTqhMeCa8Gl2kUSHP4QunM8ukxJI")
+
+# k8s.get_pods(namespace="kleiner")
+#
+# for ns in k8s.namespaces.items:
+#     logger.info(f"Namespace: {ns.metadata.name}")
+#
+# k8s.get_deployments(namespace="kleiner")
+#print(deployment)
+
+# api_instance = client.AppsV1Api()
+#
+# api_response = api_instance.create_namespaced_deployment(
+#     body=deployment,
+#     namespace=namespace
+# )
+#
+# print("Deployment created. status='%s'" % str(api_response.status))
+
+
+#k8s.create_deployment(namespace="kleiner", deployment_name="yoss", image="beam", replicas=1, port=80, cpu_request=0.1, cpu_limit=1)
+
+
+# k8s.generate_yaml_config('/tmp/my_conf.yaml', namespace='kleiner', deployment_name='yoss', image='beam',
+#                          replicas=1, port=80, cpu_request=0.1, cpu_limit=1,)
+#
+# k8s.generate_file_config('/tmp/my_conf.json', namespace='kleiner', deployment_name='yoss', image='beam',
+#
 
 #k8s.get_cpu_resources(namespace="kleiner", deployment_name="Yos")
 
--- src/beam/orchestration/k8s.py
+++ src/beam/orchestration/k8s.py
@@ -11,14 +11,20 @@ import json
 class BeamK8S(Processor):  # processor is a another class and the BeamK8S inherits the method of processor
     """BeamK8S is a class that provides a simple interface to the Kubernetes API."""
 
-    def __init__(self, api_url=None, api_token=None, *args, **kwargs):
+    def __init__(self, api_url=None, api_token=None, project_name=None, namespace=None, replicas=None, labels=None,
+                 image_name=None, deployment_name=None, ports=None, env=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.env = env
+        self.args = args
+        self.project_name = project_name
+        self.namespace = namespace
+        self.replicas = replicas
+        self.labels = labels
+        self.image_name = image_name
+        self.deployment_name = deployment_name
+        self.ports = ports
         self.api_token = api_token
         self.api_url = api_url
-        # Initialize the configuration and API clients
-        # self.configuration = self.create_configuration()
-        # self.api_client = client.ApiClient(self.configuration)
-        # self.core_v1_api = client.CoreV1Api(self.api_client)
 
     @lazy_property
     def core_v1_api(self):
@@ -39,21 +45,82 @@ class BeamK8S(Processor):  # processor is a another class and the BeamK8S inheri
         }
         return configuration
 
-    @lazy_property
-    def client(self):
+    def create_container_ports(self):
+        # Check if self.ports is a single integer and convert it to a list if so
+        ports = [self.ports] if isinstance(self.ports, int) else self.ports
+        return [client.V1ContainerPort(container_port=port) for port in ports]
 
-        # Returns CoreV1Api client
-        return client.CoreV1Api(client.ApiClient(self.configuration))
-        # configuration = Configuration()
-        # configuration.host = self.api_url
-        # configuration.verify_ssl = False  # Depends on your SSL setup
-        # configuration.debug = False
-        # configuration.api_key = {
-        #  'authorization': f"Bearer {self.api_token}"
-        # }
-        # # Create the API client
-        # # return configuration
-        # return client.CoreV1Api(client.ApiClient(configuration))
+    def create_environment_variables(self):
+        env_vars = []
+        if self.env:
+            for env_var in self.env:
+                if isinstance(env_var, dict) and 'name' in env_var and 'value' in env_var:
+                    # Ensure value is a string, convert if necessary
+                    value = str(env_var['value']) if not isinstance(env_var['value'], str) else env_var['value']
+                    env_vars.append(client.V1EnvVar(name=env_var['name'], value=value))
+                elif isinstance(env_var, str):
+                    # If env_var is a string, assume it's in "name=value" format
+                    parts = env_var.split('=', 1)
+                    if len(parts) == 2:
+                        env_vars.append(client.V1EnvVar(name=parts[0], value=parts[1]))
+                    else:
+                        # For a plain string without '=', assign a generic name
+                        env_vars.append(client.V1EnvVar(name=f"ENV_{env_var}", value=env_var))
+                elif isinstance(env_var, (int, float)):
+                    # For numeric types, convert to string and assign a generic name
+                    env_vars.append(client.V1EnvVar(name=f"NUM_ENV_{env_var}", value=str(env_var)))
+                else:
+                    raise TypeError(f"Unsupported environment variable type: {type(env_var)}")
+        return env_vars
+
+    def create_container(self):
+        container_name = f"{self.project_name}-{self.deployment_name}-container"
+        return client.V1Container(
+            name=container_name,
+            image=self.image_name,
+            ports=self.create_container_ports(),
+            args=self.args,
+            env=self.create_environment_variables()
+        )
+
+    def create_pod_template(self):
+        container = self.create_container()
+        return client.V1PodTemplateSpec(
+            metadata=client.V1ObjectMeta(labels=self.labels),
+            spec=client.V1PodSpec(containers=[container])
+        )
+
+    def create_deployment_spec(self):
+        pod_template = self.create_pod_template()
+        return client.V1DeploymentSpec(
+            print(f"Replicas before conversion: {self.replicas}"),
+            replicas=int(self.replicas),  # Cast replicas to int
+            template=pod_template,
+            selector={'matchLabels': self.labels}
+        )
+
+    def create_deployment(self):
+        deployment_spec = self.create_deployment_spec()
+        # Optionally add the project name to the deployment's metadata
+        deployment_metadata = client.V1ObjectMeta(name=self.deployment_name, namespace=self.namespace,
+                                                  labels={"project": self.project_name})
+        deployment = client.V1Deployment(
+            api_version="apps/v1",
+            kind="Deployment",
+            metadata=deployment_metadata,
+            spec=deployment_spec
+        )
+        return deployment
+
+    def apply_deployment(self):
+        deployment = self.create_deployment()
+        logger.debug(f"Deployment object to be created: {deployment}")  # Adjust logging level/method as needed
+        try:
+            apps_v1_api = client.AppsV1Api(self.api_client)
+            apps_v1_api.create_namespaced_deployment(body=deployment, namespace=self.namespace)
+            logger.info(f"Deployment {self.deployment_name} created in namespace {self.namespace}.")
+        except ApiException as e:
+            logger.exception(f"Exception when applying the deployment: {e}")
 
     @property
     def namespaces(self):
@@ -64,71 +131,6 @@ class BeamK8S(Processor):  # processor is a another class and the BeamK8S inheri
             logger.exception(f"Exception when calling CoreV1Api->list_namespace: {e}")
 
         return n_spaces
-
-    @staticmethod
-    def generate_config(namespace, deployment_name, image, replicas=1, port=80, cpu_request=0.1, cpu_limit=1,):
-        """
-        Generate a YAML configuration for a Kubernetes Deployment.
-        """
-        # Create a dictionary representation of the Deployment
-        config = {
-            'apiVersion': 'apps/v1',
-            'kind': 'Deployment',
-            'metadata': {
-                'name': deployment_name,
-                'labels': {
-                    'app': deployment_name
-                }
-            },
-            'spec': {
-                'replicas': replicas,
-                'selector': {
-                    'matchLabels': {
-                        'app': deployment_name
-                    }
-                },
-                'template': {
-                    'metadata': {
-                        'labels': {
-                            'app': deployment_name
-                        }
-                    },
-                    'spec': {
-                        'containers': [
-                            {
-                                'name': deployment_name,
-                                'image': image,
-                                'ports': [
-                                    {
-                                        'containerPort': port
-                                    }
-                                ],
-                                'resources': {
-                                    'requests': {
-                                        'cpu': f'{cpu_request}m'
-                                    },
-                                    'limits': {
-                                        'cpu': f'{cpu_limit}m'
-                                    }
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-
-        return config
-
-    def generate_yaml_config(self, path, *args, **kwargs):
-        path = beam_path(path)
-        conf = self.generate_config(*args, **kwargs)
-        path.write(conf, ext='.yaml')
-
-    def generate_file_config(self, path, *args, **kwargs):
-        path = beam_path(path)
-        conf = self.generate_config(*args, **kwargs)
-        path.write(conf)
 
     def get_deployments(self, namespace='default'):
         try:
@@ -145,49 +147,119 @@ class BeamK8S(Processor):  # processor is a another class and the BeamK8S inheri
 
     def get_pods(self, namespace='default'):
         try:
-            pods = self.client.list_namespaced_pod(namespace)
+            pods = self.core_v1_api.list_namespaced_pod(namespace)
             for pod in pods.items:
                 print(f"Pod Name: {pod.metadata.name}")
         except ApiException as e:
             print(f"Exception when calling CoreV1Api->list_namespaced_pod: {e}")
 
-    def get_cpu_resources(self, namespace, deployment_name):
-        try:
-            apps_v1_api = client.AppsV1Api(self.api_client)
-            core_v1_api = client.CoreV1Api(self.api_client)
+    # def generate_yaml_config(self, path, *args, **kwargs):
+    #     path = beam_path(path)
+    #     conf = self.generate_config(*args, **kwargs)
+    #     path.write(conf, ext='.yaml')
+    #
+    # def generate_file_config(self, path, *args, **kwargs):
+    #     path = beam_path(path)
+    #     conf = self.generate_config(*args, **kwargs)
+    #     path.write(conf)
+    # @staticmethod
+    # def generate_config(namespace, deployment_name, image, replicas=1, port=80, cpu_request=0.1, cpu_limit=1, ):
+    #     """
+    #     Generate a YAML configuration for a Kubernetes Deployment.
+    #     """
+    #     # Create a dictionary representation of the Deployment
+    #     config = {
+    #         'apiVersion': 'apps/v1',
+    #         'kind': 'Deployment',
+    #         'metadata': {
+    #             'name': deployment_name,
+    #             'labels': {
+    #                 'app': deployment_name
+    #             }
+    #         },
+    #         'spec': {
+    #             'replicas': replicas,
+    #             'selector': {
+    #                 'matchLabels': {
+    #                     'app': deployment_name
+    #                 }
+    #             },
+    #             'template': {
+    #                 'metadata': {
+    #                     'labels': {
+    #                         'app': deployment_name
+    #                     }
+    #                 },
+    #                 'spec': {
+    #                     'containers': [
+    #                         {
+    #                             'name': container.image,
+    #                             'image': image,
+    #                             'ports': [
+    #                                 {
+    #                                     'containerPort': port
+    #                                 }
+    #                             ],
+    #                             'resources': {
+    #                                 'requests': {
+    #                                     'cpu': f'{cpu_request}m'
+    #                                 },
+    #                                 'limits': {
+    #                                     'cpu': f'{cpu_limit}m'
+    #                                 }
+    #                             }
+    #                         }
+    #                     ]
+    #                 }
+    #             }
+    #         }
+    #     }
+    #
+    #     return config
+    #
+    # deploy = client.V1Container
 
-            # Get deployment
-            deployment = apps_v1_api.read_namespaced_deployment(deployment_name, namespace)
-            match_labels = deployment.spec.selector.match_labels
 
-            # List all pods matching deployment labels
-            label_selector = ",".join([f"{k}={v}" for k, v in match_labels.items()])
-            pods = core_v1_api.list_namespaced_pod(namespace, label_selector=label_selector)
 
-            total_cpu_requests = 0
-            total_cpu_limits = 0
 
-            for pod in pods.items:
-                for container in pod.spec.containers:
-                    if container.resources.requests and 'cpu' in container.resources.requests:
-                        total_cpu_requests += self.parse_cpu_value(container.resources.requests['cpu'])
-                    if container.resources.limits and 'cpu' in container.resources.limits:
-                        total_cpu_limits += self.parse_cpu_value(container.resources.limits['cpu'])
 
-            print(f"Total CPU Requests in Deployment '{deployment_name}': {total_cpu_requests}")
-            print(f"Total CPU Limits in Deployment '{deployment_name}': {total_cpu_limits}")
+    # def get_cpu_resources(self, namespace, deployment_name):
+    #     try:
+    #         apps_v1_api = client.AppsV1Api(self.api_client)
+    #         core_v1_api = client.CoreV1Api(self.api_client)
+    #
+    #         # Get deployment
+    #         deployment = apps_v1_api.read_namespaced_deployment(deployment_name, namespace)
+    #         match_labels = deployment.spec.selector.match_labels
+    #
+    #         # List all pods matching deployment labels
+    #         label_selector = ",".join([f"{k}={v}" for k, v in match_labels.items()])
+    #         pods = core_v1_api.list_namespaced_pod(namespace, label_selector=label_selector)
+    #
+    #         total_cpu_requests = 0
+    #         total_cpu_limits = 0
+    #
+    #         for pod in pods.items:
+    #             for container in pod.spec.containers:
+    #                 if container.resources.requests and 'cpu' in container.resources.requests:
+    #                     total_cpu_requests += self.parse_cpu_value(container.resources.requests['cpu'])
+    #                 if container.resources.limits and 'cpu' in container.resources.limits:
+    #                     total_cpu_limits += self.parse_cpu_value(container.resources.limits['cpu'])
+    #
+    #         print(f"Total CPU Requests in Deployment '{deployment_name}': {total_cpu_requests}")
+    #         print(f"Total CPU Limits in Deployment '{deployment_name}': {total_cpu_limits}")
+    #
+    #     except ApiException as e:
+    #         print(f"Exception when accessing Kubernetes API: {e}")
 
-        except ApiException as e:
-            print(f"Exception when accessing Kubernetes API: {e}")
-
-    def parse_cpu_value(self, cpu_value):
-        """
-        Parse Kubernetes CPU value to raw.
-        Example: '100m' -> 0.1, '1' -> 1
-        """
-        if cpu_value.endswith('m'):
-            return int(cpu_value[:-1]) / 1000
-        return int(cpu_value)
+    # def parse_cpu_value(self, cpu_value):
+    #     """
+    #     Parse Kubernetes CPU value to raw.
+    #     Example: '100m' -> 0.1, '1' -> 1
+    #     """
+    #     if cpu_value.endswith('m'):
+    #         return int(cpu_value[:-1]) / 1000
+    #     return int(cpu_value)
 
 # @lazy_property
 # def configure_kubernetes_client(self):
