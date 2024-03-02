af2cc2d8f9e4bc57522cfcc2fb73cee997c6cc59
Yossi <yossi@dayo-tech.com>
2024-03-03 01:07:05 +0200

    beampod addition : def delete_deployment def query_available_resources def list_pods adding orchestration_beampod.py adding orchestration_beamdeploy.py delete orchestration_example.py

--- examples/orchestration_beampod.py
+++ examples/orchestration_beampod.py
@@ -1,42 +1,10 @@
 # This is an example of how to use the BeamDeploy class to deploy a container to an OpenShift cluster.
-from src.beam.orchestration import BeamK8S, BeamDeploy, ServiceConfig, StorageConfig, UserIdmConfig, BeamPod
-
+from src.beam.orchestration import BeamK8S, BeamPod
 
 api_url = "https://api.kh-dev.dt.local:6443"
 api_token = "sha256~TpT3aNQgxwikdkzDBke0_v6265Kw8snnXt87-BkWIxA"
 project_name = "ben-guryon"
-image_name = "harbor.dt.local/public/beam:openshift-20.02.1"
-labels = {"app": "bgu"}
-deployment_name = "bgu"
-# namespace = "ben-guryon"
-namespace = project_name
-replicas = 1
-entrypoint_args = ["63"]  # Container arguments
-entrypoint_envs = {"TEST": "test"}  # Container environment variables
-use_scc = True  # Pass the SCC control parameter
-cpu_requests = "4"  # 0.5 CPU
-cpu_limits = "4"       # 1 CPU
-memory_requests = "24Gi"
-memory_limits = "24Gi"
-gpu_requests = "1"
-gpu_limits = "1"
-storage_configs = [
-    StorageConfig(pvc_name="data-pvc", pvc_mount_path="/data-pvc",
-                  pvc_size="500Gi", pvc_access_mode="ReadWriteMany", create_pvc=True),
-]
-service_configs = [
-    ServiceConfig(port=22, service_name="ssh", service_type="NodePort", port_name="ssh-port",
-                  create_route=False, create_ingress=False, ingress_host="ssh.example.com"),
-    ServiceConfig(port=88, service_name="jupyter", service_type="ClusterIP", port_name="jupyter-port",
-                  create_route=True, create_ingress=False, ingress_host="jupyter.example.com"),
-]
-user_idm_configs = [
-    UserIdmConfig(user_name="yos", role_name="admin", role_binding_name="yos",
-                  create_role_binding=True, project_name="ben-guryon"),
-    UserIdmConfig(user_name="asafe", role_name="admin", role_binding_name="asafe",
-                  create_role_binding=True, project_name="ben-guryon"),
-]
-
+deployment = "bgu"
 
 print('hello world')
 print("API URL:", api_url)
@@ -50,35 +18,12 @@ k8s = BeamK8S(
     namespace=project_name,
 )
 
-deployment = BeamDeploy(
-    k8s=k8s,
+pod = BeamPod(
+    api_url=api_url,
+    api_token=api_token,
     project_name=project_name,
     namespace=project_name,
-    replicas=replicas,
-    labels=labels,
-    image_name=image_name,
-    deployment_name=deployment_name,
-    use_scc=use_scc,
-    cpu_requests=cpu_requests,
-    cpu_limits=cpu_limits,
-    memory_requests=memory_requests,
-    memory_limits=memory_limits,
-    gpu_requests=gpu_requests,
-    gpu_limits=gpu_limits,
-    service_configs=service_configs,
-    storage_configs=storage_configs,
-    entrypoint_args=entrypoint_args,
-    entrypoint_envs=entrypoint_envs,
-    user_idm_configs=user_idm_configs,
+    deployment=deployment
 )
 
-deployment.launch(replicas=1)
-
-print("Fetching external endpoints...")
-internal_endpoints = k8s.get_internal_endpoints_with_nodeport(namespace=namespace)
-for endpoint in internal_endpoints:
-    print(f"Internal Access: {endpoint['node_ip']}:{endpoint['node_port']}")
-
-beam_pod_instance = deployment.launch(replicas=1)
-available_resources = beam_pod_instance.query_available_resources()
-print("Available Resources:", available_resources)
+pod.delete_deployment()
