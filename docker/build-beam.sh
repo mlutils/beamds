885b0b09fb1b5efe48ef3803fe4cc7013e5791ae
elad <elad.sarafian@gmail.com>
2024-05-28 17:21:42 +0300

    add safe imports

--- docker/build-beam.sh
+++ docker/build-beam.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 SUFFIX=$1
-MORE_ARGS=${@:2}
-
 TAG=$(date '+%Y%m%d')${SUFFIX}
 
 echo "Building image with tag: ${TAG}"
 
-docker build -f docker/Dockerfile --tag beam:${TAG} --progress=plain "$MORE_ARGS" . 2>&1 | tee -a /tmp/beam_build_$(date '+%Y%m%d').log
+docker build -f docker/Dockerfile --tag beam:${TAG} --progress=plain . 2>&1 | tee -a /tmp/beam_build_$(date '+%Y%m%d').log
