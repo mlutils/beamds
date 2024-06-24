b8e2a8e962805a4fb8ee1c17ddc1d68adcc281cc
elad <elad.sarafian@gmail.com>
2024-06-24 16:53:13 +0300

    add overwrite-symbolic-links.sh workaround

--- runs/overwrite-symbolic-links.sh
+++ runs/overwrite-symbolic-links.sh
@@ -1,14 +1,18 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
+echo "Current directory: $(pwd)"
 
 ln -s -f ../src/beam beam
 
 cd ../tests
+echo "Current directory: $(pwd)"
 ln -s -f ../src/beam beam
 
 cd ../examples
+echo "Current directory: $(pwd)"
 ln -s -f ../src/beam beam
 
 cd ../
+echo "Current directory: $(pwd)"
 ln -s -f src/beam beam
 
