0e25156bfc4060ac0f0db3f2e27c02307dd2e30f
Yossi <yossi@dayo-tech.com>
2024-06-27 11:48:27 +0300

    updating ssh config for regular user and root user

--- docker/workspace/bash-run-scripts/setup_env_vars.sh
+++ docker/workspace/bash-run-scripts/setup_env_vars.sh
@@ -5,15 +5,51 @@ declare -a vars=("NVIDIA_VISIBLE_DEVICES" "NVIDIA_REQUIRE_CUDA" "CUDA_VERSION"
                  "LD_LIBRARY_PATH" "PATH" "CUDA_HOME" "CUDA_DRIVER_VERSION"
                  "OMPI_MCA_coll_hcoll_enable" "OPAL_PREFIX")
 
-# Create or clear the existing .ssh/environment file
-echo "" > ~/.ssh/environment
+# List of users
+users=("root" "$USER_NAME")
 
-# Loop through and copy the specified environment variables
-for var in "${vars[@]}"; do
-    if [[ -v "$var" ]]; then
-        echo "$var=${!var}" >> ~/.ssh/environment
+# Loop through each user
+for user in "${users[@]}"; do
+    if [ "$user" == "root" ]; then
+        # Handle the root user specifically
+        ssh_dir="/root/.ssh"
+        echo "" > "$ssh_dir/environment"
+    else
+        # Handle other users
+        ssh_dir="$USER_HOME_DIR/.ssh"
+    fi
+
+    # Create the .ssh directory for the user if it doesn't exist
+    mkdir -p "$ssh_dir"
+
+    # Create or clear the existing .ssh/environment file for each user
+    echo "" > "$ssh_dir/environment"
+
+    # Loop through each environment variable
+    for var in "${vars[@]}"; do
+        if [[ -v "$var" ]]; then
+            # Append the environment variable to the user's .ssh/environment file
+            echo "$var=${!var}" >> "$ssh_dir/environment"
+        fi
+    done
+
+    # Ensure correct permissions are set for each user's .ssh directory and environment file
+    chmod 700 "$ssh_dir"
+    chmod 600 "$ssh_dir/environment"
+
+    if [ "$user" == "root" ]; then
+        chown -R root:root "$ssh_dir"
+    else
+        chown -R "$user:$user" "$ssh_dir"
     fi
 done
 
-# Ensure correct permissions are set
-chmod 600 ~/.ssh/environment
+# Loop through and copy the specified environment variables to root's environment file
+for var in "${vars[@]}"; do
+    if [[ -v "$var" ]]; then
+        echo "$var=${!var}" >> /root/.ssh/environment
+    fi
+done
+
+# Ensure correct permissions are set for root's .ssh environment file
+chmod 600 /root/.ssh/environment
