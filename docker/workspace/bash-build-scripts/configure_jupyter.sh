a5f274e75f2870877ee09b2f473895fd9084bf25
Yossi <yossi@dayo-tech.com>
2024-07-03 21:50:37 +0300

    fixing registry_project_name missing

--- docker/workspace/bash-build-scripts/configure_jupyter.sh
+++ docker/workspace/bash-build-scripts/configure_jupyter.sh
@@ -14,15 +14,16 @@ echo "c.LabServerApp.open_browser = False" >> /root/.jupyter/jupyter_lab_config.
 
 
 
-su - "$USER_NAME" << EOF
+if [ "$USER_NAME" != "root" ]; then
+    su - "$USER_NAME" << 'EOF'
 rm -f ~/.jupyter/jupyter_lab_config.py && jupyter-lab --generate-config
-echo "c.ServerApp.root_dir = '/home/'" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
-echo "c.ServerApp.allow_remote_access = True" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
-echo "c.ServerApp.ip = '0.0.0.0'" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
-echo "c.IdentityProvider.token = ''" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
-echo "c.ServerApp.allow_root = False" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
-echo "c.LabServerApp.open_browser = False" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
+echo "c.ServerApp.root_dir = '/home/$USER'" >> ~/.jupyter/jupyter_lab_config.py
+echo "c.ServerApp.allow_remote_access = True" >> ~/.jupyter/jupyter_lab_config.py
+echo "c.ServerApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_lab_config.py
+echo "c.ServerApp.allow_root = False" >> ~/.jupyter/jupyter_lab_config.py
+echo "c.LabApp.open_browser = False" >> ~/.jupyter/jupyter_lab_config.py
 EOF
+fi
 
 
 # language servers -experimental for future use
