b8648894c1b472f577354c33d6102ff248e689d5
Yos <yosthebos@gmail.com>
2024-07-03 18:51:02 +0300

    SSH upgarde for root ssh and user

--- docker/workspace/bash-build-scripts/configure_jupyter.sh
+++ docker/workspace/bash-build-scripts/configure_jupyter.sh
@@ -2,9 +2,9 @@
 cp /workspace/configuration/motd /etc/motd
 
 pip install jupyterlab-tensorboard-pro notebook-shim async_lru
-jupyter nbextension enable --py widgetsnbextension
+# jupyter nbextension enable --py widgetsnbextension
 
-jupyter-lab --generate-config
+rm -f ~/.jupyter/jupyter_lab_config.py && jupyter-lab --generate-config
 echo "c.ServerApp.notebook_dir = '/home/'" >> /root/.jupyter/jupyter_lab_config.py
 echo "c.ServerApp.allow_remote_access = True" >> /root/.jupyter/jupyter_lab_config.py
 echo "c.ServerApp.ip = '0.0.0.0'" >> /root/.jupyter/jupyter_lab_config.py
@@ -15,7 +15,7 @@ echo "c.LabServerApp.open_browser = False" >> /root/.jupyter/jupyter_lab_config.
 
 
 su - "$USER_NAME" << EOF
-jupyter-lab --generate-config
+rm -f ~/.jupyter/jupyter_lab_config.py && jupyter-lab --generate-config
 echo "c.ServerApp.root_dir = '/home/'" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
 echo "c.ServerApp.allow_remote_access = True" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
 echo "c.ServerApp.ip = '0.0.0.0'" >> "$USER_HOME_DIR/.jupyter/jupyter_lab_config.py"
