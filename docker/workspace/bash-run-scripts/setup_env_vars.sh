0169db62902379d51d209f066796657e949bef87
Yossi <yossi@dayo-tech.com>
2024-07-07 14:52:27 +0300

    fixing env with sourcing from root profile to ssh profile

--- docker/workspace/bash-run-scripts/setup_env_vars.sh
+++ docker/workspace/bash-run-scripts/setup_env_vars.sh
@@ -5,59 +5,50 @@ declare -a vars=("NVIDIA_VISIBLE_DEVICES" "NVIDIA_REQUIRE_CUDA" "CUDA_VERSION"
                  "LD_LIBRARY_PATH" "PATH" "CUDA_HOME" "CUDA_DRIVER_VERSION"
                  "OMPI_MCA_coll_hcoll_enable" "OPAL_PREFIX")
 
-# Loop through each environment variable in the array
-for var in "${vars[@]}"; do
-    # Print the variable name
-    echo "Variable name: $var"
-    # Print the variable value
-    echo "Variable value: ${!var}"
-done
-
 # List of users
 users=("root" "$USER_NAME")
 
-# Loop through each user
 for user in "${users[@]}"; do
     if [ "$user" == "root" ]; then
         # Handle the root user specifically
         ssh_dir="/root/.ssh"
-        echo "" > "$ssh_dir/environment"
+        profile_file="/root/.profile"
     else
         # Handle other users
         ssh_dir="$USER_HOME_DIR/.ssh"
+        profile_file="$USER_HOME_DIR/.profile"
     fi
 
     # Create the .ssh directory for the user if it doesn't exist
     mkdir -p "$ssh_dir"
 
-    # Create or clear the existing .ssh/environment file for each user
-    echo "" > "$ssh_dir/environment"
+    # File to store environment variable settings
+    env_file="$ssh_dir/set_env.sh"
+    echo "#!/bin/bash" > "$env_file"
 
     # Loop through each environment variable
     for var in "${vars[@]}"; do
         if [[ -v "$var" ]]; then
-            # Append the environment variable to the user's .ssh/environment file
-            echo "$var=${!var}" >> "$ssh_dir/environment"
+            # Append the environment variable export to the script file
+            echo "export $var='${!var}'" >> "$env_file"
         fi
     done
 
-    # Ensure correct permissions are set for each user's .ssh directory and environment file
+    # Make the environment script executable
+    chmod +x "$env_file"
+
+    # Append source command to .profile or .bashrc to source the variables on login
+    echo "source '$env_file'" >> "$profile_file"
+
+    # Ensure correct permissions are set for each user's .ssh directory and profile file
     chmod 700 "$ssh_dir"
-    chmod 600 "$ssh_dir/environment"
+    chmod 600 "$profile_file"
 
     if [ "$user" == "root" ]; then
         chown -R root:root "$ssh_dir"
+        chown root:root "$profile_file"
     else
         chown -R "$user:$user" "$ssh_dir"
+        chown "$user:$user" "$profile_file"
     fi
 done
-
-# Loop through and copy the specified environment variables to root's environment file
-for var in "${vars[@]}"; do
-    if [[ -v "$var" ]]; then
-        echo "$var=${!var}" >> /root/.ssh/environment
-    fi
-done
-
-# Ensure correct permissions are set for root's .ssh environment file
-chmod 600 /root/.ssh/environment
