b1e048f897ba22824c7c962452e03e5307084a2f
Yossi <yossi@dayo-tech.com>
2024-08-10 15:05:07 +0300

    testing alg and deploy

--- examples/orchestration_debug_demo.py
+++ examples/orchestration_debug_demo.py
@@ -1,10 +1,10 @@
 from beam import resource
 from beam.serve import beam_server
 from beam import deploy_server
-from beam import HTTPServeClusterConfig
+from beam import ServeClusterConfig
 
 
 alg = resource('/tmp/yolo-bundle/').read(ext='.abm')
 beam_server(alg, non_blocking=True)
-conf = HTTPServeClusterConfig('/home/beamds/examples/orchestration_beamdemo.yaml')
+conf = ServeClusterConfig('/home/beamds/examples/orchestration_beamdemo.yaml')
 deploy_server('/tmp/yolo-bundle/', conf)
