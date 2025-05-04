0296c6aed7ca3a9d53c5c5e80979f976a8460b33
elad <elad.sarafian@gmail.com>
2025-05-04 12:44:58 +0300

    optimize beam image

--- container/workspace/bash-build-scripts/apt_installs.sh
+++ container/workspace/bash-build-scripts/apt_installs.sh
@@ -130,3 +130,6 @@ touch ~/.ssh/environment
 # ssh connection immediately disconnects after session start with exit code 254:
 # https://unix.stackexchange.com/questions/148714/cant-ssh-connection-terminates-immediately-with-exit-status-254
 sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+
+# delete cache files
+apt-get clean
